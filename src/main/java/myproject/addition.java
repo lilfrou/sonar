d503657ccf40e401bc161d643dbb05db89f6e5df
lilfrou <51007021+lilfrou@users.noreply.github.com>
2019-08-06 15:47:19 +0100

    Update addition.java

--- src/main/java/myproject/addition.java
+++ src/main/java/myproject/addition.java
@@ -4,5 +4,8 @@ public class addition {
 	public static int add( int operand1, int operand2 ) {
 		 return operand1 + operand2;
 		 }
+	public static int div( int operand1 , int operand2 ) {
+		return operand1 / operand2;
+		}
 
 }
